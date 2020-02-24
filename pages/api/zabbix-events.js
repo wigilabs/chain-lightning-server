c522c67857538e3681c9b50d8feccaf847f8cbbc
oil <le.rue@protonmail.com>
2020-02-24 18:21:44 -0500

    all in one

--- pages/api/zabbix-events.js
+++ pages/api/zabbix-events.js
@@ -1,50 +1,28 @@
 const superagent = require('superagent');
 const handler = (req, res) => {
   
-  let event1
-  let event2
-  let event3
+  let event
+
   
   superagent
-    .get('https://insights.newrelic.com/accounts/2482859/query?query=SELECT+*+FROM+ZabbixHTTP')
+    .get('https://insights.newrelic.com/accounts/2482859/query?query=SELECT+description+FROM+ZabbixAgent,+ZabbixHTTP,+ZabbixSSH+limit+3')
     .set('X-Query-Key', 'NRIQ-j7zJFckfarn9jsOB-nPscK5H6z0QCgHd')
     .set('Accept', 'application/json')
     .end((err, response) => {
       events = JSON.parse(response.text).results[0].events
       console.log('events: ', events)
-      event1 = [
-          "name": "SSH",
-          "value": 1
-        ]
+      event = [{
+          name: 'SSH',
+          value: 1
+        }, {
+            name: 'HTTP',
+            value: 1
+        }, {
+            name: 'Agent',
+            value: 1
+        }]
       return res.json(events);
     });
     
-    superagent
-    .get('https://insights.newrelic.com/accounts/2482859/query?query=SELECT+*+FROM+ZabbixSSH')
-    .set('X-Query-Key', 'NRIQ-j7zJFckfarn9jsOB-nPscK5H6z0QCgHd')
-    .set('Accept', 'application/json')
-    .end((err, response) => {
-      events = JSON.parse(response.text).results[0].events
-      console.log('events: ', events)
-      event2 = [
-       "name": "HTTP",
-       "value": 1
-        ]
-      return res.json(events);
-    });
-  
-    superagent
-    .get('https://insights.newrelic.com/accounts/2482859/query?query=SELECT+*+FROM+ZabbixAgent')
-    .set('X-Query-Key', 'NRIQ-j7zJFckfarn9jsOB-nPscK5H6z0QCgHd')
-    .set('Accept', 'application/json')
-    .end((err, response) => {
-      events = JSON.parse(response.text).results[0].events
-      console.log('events: ', events)
-      event3 = [
-        "name" : "Agent",
-        "value" : 1
-        ]
-      return res.json(events);
-    });
 };
 export default handler;
