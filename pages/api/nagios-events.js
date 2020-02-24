bbe8b51b5647ee03b5fca86b5d4d62a185f1670d
Dieter Evangelista <hansevangelista@gmail.com>
2020-02-24 14:23:27 -0500

    add new version

--- pages/api/nagios-events.js
+++ pages/api/nagios-events.js
@@ -12,6 +12,17 @@ const handler = (req, res) => {
       events = JSON.parse(response.text).results[0].events
       console.log('events: ', events)
 
+      events = [
+          {name: 'Current Load',    value: 1},
+          {name: 'Current Users',   value: 1},
+          {name: 'HTTP',            value: 0},
+          {name: 'PING',            value: 1},
+          {name: 'Root Partition',  value: 1},
+          {name: 'SSH',             value: 0},
+          {name: 'Swap Usage',      value: 1},
+          {name: 'Total Processes', value: 1},
+        ]
+
       return res.json(events);
     });
 };
