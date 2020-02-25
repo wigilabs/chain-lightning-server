eebc1604a114ead29b580df957730b102cbb8ee9
oil <le.rue@protonmail.com>
2020-02-24 20:16:13 -0500

    data----

---
+++ .next/server/static/UOfH9YHouC2ReprT4dBmY/pages/api/zabbix-events.js
@@ -0,0 +1,153 @@
+module.exports =
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = require('../../../../ssr-module-cache.js');
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete installedModules[moduleId];
+/******/ 		}
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 1:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("VJV/");
+
+
+/***/ }),
+
+/***/ "VJV/":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const superagent = __webpack_require__("iHmL");
+
+const handler = (req, res) => {
+  let events;
+  superagent.get('https://insights-api.newrelic.com/v1/accounts/2482859/query?nrql=SELECT%20description%20FROM%20ZabbixAgent,%20ZabbixHTTP,%20ZabbixSSH%20limit%203').set('X-Query-Key', 'NRIQ-j7zJFckfarn9jsOB-nPscK5H6z0QCgHd').set('Accept', 'application/json').end((err, response) => {
+    events = JSON.parse(response.text).results[0].events;
+    console.log('events: ', events);
+    events.forEach((element, index, array) => {
+      console.log(element.timestamp); // 100, 200, 300
+
+      console.log(index); // 0, 1, 2
+
+      console.log(array); // same myArray object 3 times
+    });
+    /*events = [
+      {
+        name: 'SSH',
+        value: 1
+      }, 
+      {
+        name: 'HTTP',
+        value: 1
+      }, 
+      {
+        name: 'Agent',
+        value: 1
+      }
+            ]*/
+
+    return res.json(events);
+  });
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (handler);
+
+/***/ }),
+
+/***/ "iHmL":
+/***/ (function(module, exports) {
+
+module.exports = require("superagent");
+
+/***/ })
+
+/******/ });
--- pages/api/zabbix-events.js
+++ pages/api/zabbix-events.js
@@ -1,17 +1,29 @@
 const superagent = require('superagent');
 const handler = (req, res) => {
-  
-  let event
-
-  
+  let events
   superagent
-    .get('https://insights.newrelic.com/accounts/2482859/query?query=SELECT+description+FROM+ZabbixAgent,+ZabbixHTTP,+ZabbixSSH+limit+3')
+    .get('https://insights-api.newrelic.com/v1/accounts/2482859/query?nrql=SELECT%20description%20FROM%20ZabbixAgent,%20ZabbixHTTP,%20ZabbixSSH%20limit%203')
     .set('X-Query-Key', 'NRIQ-j7zJFckfarn9jsOB-nPscK5H6z0QCgHd')
     .set('Accept', 'application/json')
     .end((err, response) => {
-      events = JSON.parse(response.text).results[0].events
+     events = JSON.parse(response.text).results[0].events
       console.log('events: ', events)
-      event = [
+      var self = this
+     events.forEach((element, index, array) => {
+    console.log("=====================================================================")
+    console.log(index) 
+         delete element.timestamp
+         switch (index) {
+             case 0 : //ssh
+                   element.name=index
+             case 1 : //http
+                   element.name=index
+             case 2 : //agent
+                   element.name=index
+                        }
+    console.log("=====================================================================")
+});
+      /*events = [
         {
           name: 'SSH',
           value: 1
@@ -24,10 +36,8 @@ const handler = (req, res) => {
           name: 'Agent',
           value: 1
         }
-      ]
-      
+              ]*/
       return res.json(events);
     });
-    
 };
 export default handler;
